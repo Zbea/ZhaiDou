e67813a6f6648de51d51c2746ddf1e6e91347173
roy <xf.radish@gmail.com>
2015-07-15 20:33:59 +0800

    首页广告循环播放，添加第三方图片异步加载

--- app/src/main/java/com/zhaidou/ZDApplication.java
+++ app/src/main/java/com/zhaidou/ZDApplication.java
@@ -2,6 +2,16 @@ package com.zhaidou;
 
 import android.app.Application;
 import android.graphics.Typeface;
+import android.os.Environment;
+
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
+import com.nostra13.universalimageloader.cache.memory.impl.UsingFreqLimitedMemoryCache;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.utils.StorageUtils;
+import com.zhaidou.utils.ToolUtils;
+
+import java.io.File;
 
 /**
  * Created by wangclark on 15/7/2.
@@ -12,7 +22,44 @@ public class ZDApplication extends Application{
     public void onCreate() {
         super.onCreate();
         initTypeFace();
+
+        creatFile();
+        setImageLoad();
     }
+
+    /**
+     * universal_image_loader基本配置
+     */
+    private void setImageLoad()
+    {
+        File cacheDir = StorageUtils.getOwnCacheDirectory(getApplicationContext(), "zhaidou/image_cache/");
+        ImageLoaderConfiguration configuration = new  ImageLoaderConfiguration.Builder(this)
+                .threadPoolSize(5)//线程池加载的数量
+                .diskCacheFileCount(100)//最大缓存数量
+                .diskCache(new UnlimitedDiscCache(cacheDir))//设置缓存路径
+                .memoryCache(new UsingFreqLimitedMemoryCache(2 * 1024 * 1024))
+                .build();
+        ImageLoader.getInstance().init(configuration);
+    }
+
+    private void creatFile()
+    {
+        String f;
+        if (ToolUtils.hasSdcard())
+        {
+            f = Environment.getExternalStorageDirectory().getAbsolutePath() + "/zhaidou/";
+        }
+        else
+        {
+            f = getFilesDir().getAbsolutePath() + "/zhaidou/";
+        }
+        File file = new File(f);
+        if (!file.exists())
+        {
+            file.mkdirs();
+        }
+    }
+
 
     private void initTypeFace(){
         if (mTypeFace==null){
---
+++ app/src/main/java/com/zhaidou/utils/ToolUtils.java
@@ -0,0 +1,61 @@
+package com.zhaidou.utils;
+
+import android.os.Environment;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.zhaidou.R;
+
+/**
+ * Created by roy on 15/7/15.
+ */
+public class ToolUtils
+{
+    /**
+     * 是否存在sdcard
+     * @return
+     */
+    public static boolean hasSdcard()
+    {
+        String state = Environment.getExternalStorageState();
+        if (state.equals(Environment.MEDIA_MOUNTED) && Environment.getExternalStorageDirectory().exists())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    /**
+     * 图片异步加载（缓存图片方法）
+     * @param url
+     * @param imageView
+     */
+    public static final void setImageCacheUrl(String url,ImageView imageView)
+    {
+        DisplayImageOptions options=new DisplayImageOptions.Builder()
+                //	.displayer(new RoundedBitmapDisplayer(20))//设置圆角半径
+                .showImageForEmptyUri(R.drawable.icon_loading_defalut)
+                .showImageOnFail(R.drawable.icon_loading_defalut)
+                .resetViewBeforeLoading(true)//default 设置图片在加载前是否重置、复位
+                .cacheInMemory(true) // default  设置下载的图片是否缓存在内存中
+                .cacheOnDisk(true) // default  设置下载的图片是否缓存在SD卡中
+                .build();
+        ImageLoader.getInstance().displayImage(url, imageView,options);
+    }
+
+    /**
+     * 图片异步加载（不缓存图片设置）
+     * @param url
+     * @param imageView
+     */
+    public static final void setImageUrl(String url,ImageView imageView)
+    {
+        ImageLoader.getInstance().displayImage(url, imageView);
+    }
+
+}
