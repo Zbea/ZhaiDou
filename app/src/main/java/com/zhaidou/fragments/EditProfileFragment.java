95189fe8885bca97f1d51dd37aeef5e9d8d8ec68
wangclark <jpwang@zhaidou.com>
2015-07-17 20:22:30 +0800

    bug fixed

--- app/src/main/java/com/zhaidou/fragments/EditProfileFragment.java
+++ app/src/main/java/com/zhaidou/fragments/EditProfileFragment.java
@@ -57,7 +57,6 @@ import java.util.Map;
  * create an instance of this fragment.
  *
  */
-
 public class EditProfileFragment extends BaseFragment implements View.OnClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
