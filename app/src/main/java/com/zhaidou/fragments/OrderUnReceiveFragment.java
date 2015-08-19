a2bc452b6be42edba6f4cd0d0b7c0a904d7b0b09
wangclark <jpwang@zhaidou.com>
2015-08-19 15:44:58 +0800

    同步

--- app/src/main/java/com/zhaidou/fragments/OrderUnReceiveFragment.java
+++ app/src/main/java/com/zhaidou/fragments/OrderUnReceiveFragment.java
@@ -275,7 +275,6 @@ public class OrderUnReceiveFragment extends BaseFragment {
         });
     }
 
-
     private void FetchReceiveData() {
         JsonObjectRequest request = new JsonObjectRequest(ZhaiDou.URL_ORDER_LIST + "?status=1,4", new Response.Listener<JSONObject>() {
             @Override
