3db5984aab81b3d2fb8e9e701fe668aa2ecfdb3f
TFanAiZhong <xiaofeng9212@126.com>
2015-11-10 17:09:27 +0800

    更新

--- app/src/main/java/com/zhaidou/MainActivity.java
+++ app/src/main/java/com/zhaidou/MainActivity.java
@@ -203,7 +203,7 @@ public class MainActivity extends BaseActivity implements DiyFragment.OnFragment
         getVersionServer();
 
         initComponents();
-//        commitActiveData();
+        commitActiveData();
         AlibabaSDK.asyncInit(this, new InitResultCallback()
         {
             @Override
